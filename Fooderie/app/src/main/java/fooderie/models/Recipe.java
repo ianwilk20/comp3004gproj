6e79901311f89a2c287dc447e036ff1c090ca7c1
GavinJack <33667483+GavinJack@users.noreply.github.com>
2020-02-04 12:02:55 -0500

    Revert "Gavin branch"

--- Fooderie/app/src/main/java/fooderie/models/Recipe.java
+++ Fooderie/app/src/main/java/fooderie/models/Recipe.java
@@ -1,21 +1,4 @@
 package fooderie.models;
 
-import androidx.room.ColumnInfo;
-import androidx.room.Entity;
-import androidx.room.Index;
-import androidx.room.PrimaryKey;
-
-@Entity (tableName = "table_Recipe",
-         indices = {@Index("recipe_id")})
 public class Recipe {
-    @PrimaryKey(autoGenerate = true)
-    @ColumnInfo(name="recipe_id")
-    private int id;
-
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
 }
