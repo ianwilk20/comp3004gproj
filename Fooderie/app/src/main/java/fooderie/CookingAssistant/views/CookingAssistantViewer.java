50360023a8d4dce9a18335ad12d52aad14e81d6b
Michael Ting <michaelt604@gmail.com>
2020-02-05 22:38:25 -0500

    Fixed recipe parsing

--- Fooderie/app/src/main/java/fooderie/CookingAssistant/views/CookingAssistantViewer.java
+++ Fooderie/app/src/main/java/fooderie/CookingAssistant/views/CookingAssistantViewer.java
@@ -9,17 +9,18 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
+import android.widget.TextView;
 import java.io.IOException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
 import org.jsoup.select.Elements;
 
 public class CookingAssistantViewer extends AppCompatActivity
 {
     private static final String TAG = CookingAssistantViewer.class.getSimpleName();
-
+    TextView tstTestView;
     ListView insList;
     String instructionList[] = {"1. Boil Water", "2. Open Raman Pack", "3. Pour half of water into a bowl", "4. Put Raman into boiling water to cook", "5. Put flavour packets into bowl water", "6. Wait until Raman cooked", "7. Strain raman and put into bowl"};
 
@@ -28,6 +29,7 @@ public class CookingAssistantViewer extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cooking_assistant_viewer);
+        tstTestView = findViewById(R.id.tempList);
 
         Log.d(TAG,"\n\n\n ----------------");
         new jSoupParse().execute();
@@ -42,6 +44,7 @@ public class CookingAssistantViewer extends AppCompatActivity
     {
         String words;
         String title;
+        String listText = "";
 
         @Override
         protected Void doInBackground(Void... voids)
@@ -50,15 +53,23 @@ public class CookingAssistantViewer extends AppCompatActivity
             {
                 Document doc = Jsoup.connect("https://food52.com/recipes/22633-strawberry-basil-lemonade").get();
                 //Elements recipe = doc.select("#recipeDirectionsRoot > div:eq(1) > ol");
-                Elements recipe = doc.select("ol");
+                Elements recipeList = doc.select("ol");
 
-                for (Element element : recipe)
+                int count = 0;
+                for (Element recipeL : recipeList)
                 {
-                    Log.d(TAG, element.html());
-                }
+                    for(int i = 1; i < recipeL.childNodeSize(); i += 2) //Skip over the empty child nodes
+                    {
+                        count += 1;
 
-                title = doc.title();
-                words = doc.text();
+                        Node step1 = recipeL.childNode(i);
+                        Node step2 = step1.childNode(1);
+                        Node step3 = step2.childNode(0);
+                        String val = step3.toString();
+                        listText += count + ". " + val + "\n\n";
+
+                    }
+                }
 
             }
             catch(Exception e)
@@ -74,7 +85,7 @@ public class CookingAssistantViewer extends AppCompatActivity
         protected void onPostExecute(Void aVoid) {
             super.onPostExecute(aVoid);
             Log.d(TAG, "done");
-            //Log.d(TAG, title);
+            tstTestView.setText(listText);
 
         }
     }
