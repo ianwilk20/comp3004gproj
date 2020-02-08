2c7c1c0cfd231a05c67a12f632df137c54eaaac1
ianwilk20 <ianwilk20@gmail.com>
2020-02-08 15:44:38 -0500

    Week 3 Workload - Ian

---
+++ Fooderie/app/src/main/java/fooderie/groceryList/IngredientNotFoundException.java
@@ -0,0 +1,9 @@
+package fooderie.groceryList;
+
+public class IngredientNotFoundException extends Exception {
+
+    public IngredientNotFoundException(String ex_message){
+        super(ex_message);
+    }
+
+}
