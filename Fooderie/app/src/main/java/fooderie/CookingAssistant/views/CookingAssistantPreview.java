01306f31d0b92e20b46454966c7c010df86f81ca
macattacksleep <59788154+macattacksleep@users.noreply.github.com>
2020-02-24 13:56:41 -0500

    Deleted RecipeIngredient class

--- Fooderie/app/src/main/java/fooderie/CookingAssistant/views/CookingAssistantPreview.java
+++ Fooderie/app/src/main/java/fooderie/CookingAssistant/views/CookingAssistantPreview.java
@@ -4,18 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.fooderie.R;
-import com.squareup.picasso.Picasso;
-
-import java.util.ArrayList;
 
 import fooderie.recipeBrowser.models.Recipe;
-import fooderie.recipeBrowser.models.RecipeIngredient;
 
 
 public class CookingAssistantPreview extends AppCompatActivity
--- Fooderie/app/src/main/java/fooderie/recipeBrowser/models/RecipeIngredient.java
+++
@@ -1,9 +0,0 @@
-package fooderie.recipeBrowser.models;
-
-import java.io.Serializable;
-
-public class RecipeIngredient implements Serializable{
-
-    public String text;
-    public double weight;
-}
